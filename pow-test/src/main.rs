3099653a2bd5c8ca0a3a41092974dc4b5e6b0f95
Toni Feric <toni.feric@belsoft.ch>
2023-05-15 13:28:24 +0200

    Try truncation to 64bit for performance reasons

--- pow-test/src/main.rs
+++ pow-test/src/main.rs
@@ -4,9 +4,19 @@ use sha2::{Digest, Sha256};
 
 fn main() {
     const DIFFICULTY: Uint<256, 4> = uint!(0b0011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111_U256);
-    println!("My 256-bit number: {:x}", DIFFICULTY);
+    println!("My difficulty:         {:x}", DIFFICULTY);
     let mut rng = rand::thread_rng();
     let random_number: u128 = rng.gen();
-    let myhash = Sha256::digest(&random_number.to_be_bytes());
-    println!("My hash:           {:x}", myhash);
+
+    let mut myhash = Sha256::new();
+    myhash.update(random_number.to_be_bytes());
+    let mut myhash_str: String = format!("{:x}", myhash.finalize());
+    println!("My hash:               {}", myhash_str);
+    let mut difficulty_str: String = format!("{:x}", DIFFICULTY);
+
+    difficulty_str.truncate(8);
+    myhash_str.truncate(8);
+    println!("My difficulty (trunc): {}", difficulty_str);
+    println!("My hash (trunc):       {}", myhash_str);
+
 }
